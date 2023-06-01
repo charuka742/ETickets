ca0001ce1e951e0b64a9fea9fcde59a1ad04a01f
Disanayaka AAC <charair742@gmail.com>
2023-06-01 10:54:57 +0530

    Add project files.

---
+++ ETickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ETickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
